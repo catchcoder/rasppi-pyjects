34c4afe43fda7219924124459b06b19c6baf898e
catchcoder <catchcoder@gmail.com>
2014-10-28 20:56:06 +0000

    Flash Leds via NPN transistor

---
+++ 01_flashledswith2n3094.py
@@ -0,0 +1,21 @@
+# leds.py
+# flash eds vi 2n3904
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led_pin = 22
+led_pause = 0.2
+
+GPIO.setup(led_pin, GPIO.OUT)
+
+try:         
+    while True:
+        GPIO.output(led_pin, True)
+        time.sleep(led_pause)
+        GPIO.output(led_pin, False)
+        time.sleep(led_pause)
+finally:  
+    print("Cleaning up")
+    GPIO.cleanup()
