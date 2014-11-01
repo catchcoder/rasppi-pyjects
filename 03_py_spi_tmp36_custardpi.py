d153735c79d084ef16ea1c9ef659081fd83db07c
catchcoder <catchcoder@gmail.com>
2014-11-01 10:23:07 +0000

    ADC with custoardPi and LLMP

---
+++ 03_py_spi_tmp36_custardpi.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+ 
+import spidev
+import time
+ 
+spi = spidev.SpiDev()
+spi.open(0,0)
+ 
+# read SPI data from one of the MCP3008's eight possible inputs (0 thru 7)
+def readadc(adcnum):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        r = spi.xfer2([1,(8+adcnum)<<4,0])
+	#print r
+	#print str([1,(8+adcnum)<<4,0])
+        adcout = ((r[1]&3) << 8) + r[2]
+        return adcout
+ 
+# TMP36 temperature sensor connected to ADC input 0
+TempSensor = 0
+ 
+while True:
+        rawTemp = readadc(TempSensor)
+        # Convert the raw ADC input to milliVolts, degrees Celsius and Fahrenheit
+        milliVolts = rawTemp * (3300.0 / 4096.0)
+        tempCelsius = ((milliVolts - 100.0) / 10.0) - 40.0
+        tempFahrenheit = (tempCelsius * 9.0 / 5.0 ) + 32
+	print ('-----') 
+        print "raw temp=", rawTemp
+        print "mV=", milliVolts 
+	print "Celsius=", int(tempCelsius)
+        print "Fahrenheit=", int(tempFahrenheit)
+ 
+        time.sleep(60)
---
+++ 05_py_spi_tmp36_web.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+ 
+import spidev
+import time
+import datetime
+ 
+spi = spidev.SpiDev()
+spi.open(0,0)
+ 
+# read SPI data from one of the ADC chips such as MCP3008's eight possible inputs (0 thru 7)
+def readadc(adcnum):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        r = spi.xfer2([1,(8+adcnum)<<4,0])
+	#print r
+	#print str([1,(8+adcnum)<<4,0])
+        adcout = ((r[1]&3) << 8) + r[2]
+        return adcout
+ 
+# TMP36 temperature sensor connected to ADC input 0
+TempSensor = 0
+temp = 0
+while True:
+        rawTemp = readadc(TempSensor)
+        # Convert the raw ADC input to milliVolts, degrees Celsius and Fahrenheit
+        milliVolts = rawTemp * (3300.0 / 4096.0)
+        tempCelsius = int(((milliVolts - 100.0) / 10.0) - 40.0)
+        #tempFahrenheit = (tempCelsius * 9.0 / 5.0 ) + 32
+	if temp <> tempCelsius:
+		print ("Temperature %sc" % str(tempCelsius))
+		temp = tempCelsius
+		d = datetime.datetime.now().strftime("%H:%M:%S on %d-%m-%Y")
+		f = open("/var/www/temp.txt","w")
+		f.write (str(temp)+ "\n")
+		f.write (str(d))
+		f.close()
+
+	#print ('-----') 
+        #print "raw temp=", rawTemp
+        #print "mV=", milliVolts 
+	#print "Celsius=", int(tempCelsius)
+        #print "Fahrenheit=", int(tempFahrenheit)
+ 
+        time.sleep(0.1)
